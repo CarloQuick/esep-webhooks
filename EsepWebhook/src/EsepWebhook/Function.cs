e0047265a2c30fdd2345bed4eb68909577b2e07c
Carlo Quick <98628846+CarloQuick@users.noreply.github.com>
2023-11-10 10:37:00 -0500

    Lambda Redeploy

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,8 +1,43 @@
-using System.Runtime.InteropServices;
+// using System.Runtime.InteropServices;
+// using System.Text;
+// using System.Text.Json.Serialization;
+// using Amazon.Lambda.Core;
+// using Microsoft.VisualBasic;
+// using Newtonsoft.Json;
+
+// // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+// [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+// namespace EsepWebhook;
+
+// public class Function
+// {
+    
+//     /// <summary>
+//     /// A simple function that takes a string and does a ToUpper
+//     /// </summary>
+//     /// <param name="input"></param>
+//     /// <param name="context"></param>
+//     /// <returns></returns>
+//     public string FunctionHandler(string input, ILambdaContext context)
+//     {
+//         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+
+//         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+//         var client = new HttpClient();
+//         var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL")){
+//             Content = new StringContent(payload, Encoding.UTF8, "application/json")
+//         };
+//         var response = client.Send(webRequest);
+//         using var reader = new StreamReader(response.Content.ReadAsStream());
+        
+//         return reader.ReadToEnd(); 
+//     // return input.ToUpper();
+//     }
+    
+// }
 using System.Text;
-using System.Text.Json.Serialization;
 using Amazon.Lambda.Core;
-using Microsoft.VisualBasic;
 using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -19,20 +54,21 @@ public class Function
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(object input, ILambdaContext context)
     {
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-
+        
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+        
         var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL")){
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
+
         var response = client.Send(webRequest);
         using var reader = new StreamReader(response.Content.ReadAsStream());
-        
-        return reader.ReadToEnd(); 
-    // return input.ToUpper();
+            
+        return reader.ReadToEnd();
     }
-    
 }
