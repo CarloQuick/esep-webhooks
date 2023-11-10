14b1513c1c1bcc4710fa8115fb4ce68b0687a6bd
Carlo Quick <98628846+CarloQuick@users.noreply.github.com>
2023-11-10 10:29:46 -0500

    Redeploy Lambda

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -32,7 +32,7 @@ public class Function
         using var reader = new StreamReader(response.Content.ReadAsStream());
         
         return reader.ReadToEnd(); 
-
+    // return input.ToUpper();
     }
     
 }
